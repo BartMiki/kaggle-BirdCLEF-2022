304aa912b0eee006c6b5129292ea5ca3c08b7fda
Bartosz Mikulski <bartmiki97@gmail.com>
2022-05-11 19:29:34 +0200

    Environment update
    * Spectrogram inspection
    * Env update

---
+++ sound.py
@@ -0,0 +1,22 @@
+import torchaudio
+from torchaudio.transforms import Spectrogram, Resample
+import matplotlib.pyplot as plt
+import torch
+
+# https://enzokro.dev/spectrogram_normalizations/2020/09/10/Normalizing-spectrograms-for-deep-learning.html
+
+audio, sr = torchaudio.load('data/train_audio/afrsil1/XC125458.ogg')
+audio = audio[:, sr*10:sr*15]
+
+print(audio.max(), audio.min(), audio.mean(), audio.std())
+print(audio.shape)
+print(sr)
+
+print(audio.shape[1] / sr)
+
+audio = Resample(sr, 8000)(audio)
+spectrogram = Spectrogram()(audio)[0]
+
+plt.imshow(torch.log(spectrogram))
+plt.title(f"Spectrogram of size: {spectrogram.shape}")
+plt.savefig('spectrogram.png')
